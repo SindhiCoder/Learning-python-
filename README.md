2a20e3eb99c43e3bd52c771bbf6197b457ecbbf8
SindhiCoder <sindhcoder@gmail.com>
2020-12-25 22:13:48 +0500

    Second Video Code

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# Today's Topic is
-
-## Python Syntax
-    
-- Filename
-- indentation
-- Comments
-- Variables
-- Spelling
-- Python Data types
-    - Numeric Data types
-    - String
-    - Boolean
+# Learning Python in Sindhi
+1. Setting Up Environment for Python
+-----------------------------------------
+2. Simple concepts
+    - Variables
+    - Python data Types 
+        - Numeric Data types
+        - String
+        - Boolean
+    - Python Comments
+-------------------------------------------
---
+++ Second.py
@@ -0,0 +1,19 @@
+# Name = Value
+Var1 = 12 # integer
+# we can change Variable value.
+print("Before Change", Var1)
+Var1 = 6 
+print("After Change ", Var1)
+# 12.0983 Floating points Numbers 
+Var3 = 12.90 # Floating Point Numbers
+# we can get the type of any Variable we have assigned, by passing Variable name as Arrgument to the 'type' function in python
+print('type of Var3', type(Var3))
+
+print("Type of Var1 ", type(Var1))
+# String(text) can be created by providing the text into Qoutation marks, like blow text suggests
+St1 = 'String Value'
+print(St1)
+# boolean Data type has to Value either True or False
+Bol =True
+Bol2 = False
+
